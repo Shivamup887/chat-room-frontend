819dd558bd16fc658d656c194c1927ce247c1d00
Shivam Upadhyay <64646989+Shivamup887@users.noreply.github.com>
2026-01-12 19:43:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const params = new URLSearchParams(window.location.search);
+const roomId = params.get("room");
+
+const socket = io("http://localhost:3000");
+
+socket.emit("join-room", roomId);
+
+socket.on("message", (msg) => {
+  const div = document.createElement("div");
+  div.innerText = msg;
+  document.getElementById("messages").appendChild(div);
+});
+
+function sendMessage() {
+  const input = document.getElementById("msg");
+  socket.emit("chat-message", input.value);
+  input.value = "";
+}
