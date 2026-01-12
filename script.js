91d23984e145f9186d63c5287672908e8e6159d6
Shivam Upadhyay <64646989+Shivamup887@users.noreply.github.com>
2026-01-12 21:40:24 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 const params = new URLSearchParams(window.location.search);
 const roomId = params.get("room");
 
-const socket = io("https://chat-room-backend-w2ag.onrender.com");
+const socket = io("http://localhost:3000");
 
 socket.emit("join-room", roomId);
 
-socket.on("message", (msg) => {
+socket.on("message", (data) => {
   const div = document.createElement("div");
-  div.innerText = msg;
+  div.classList.add("message");
+
+  if (data.senderId === socket.id) {
+    div.classList.add("right");
+  } else {
+    div.classList.add("left");
+  }
+
+  div.innerText = data.text;
   document.getElementById("messages").appendChild(div);
+
+  // auto-scroll
+  document.getElementById("messages").scrollTop =
+    document.getElementById("messages").scrollHeight;
 });
+
 
 function sendMessage() {
   const input = document.getElementById("msg");
   socket.emit("chat-message", input.value);
   input.value = "";
 }
-
